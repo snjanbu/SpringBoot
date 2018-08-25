fdca8357ba586758a071c676a66740254abe17c8
Sanjayanbu <snjanbu@gmail.com>
2018-08-25 20:03:31 +0530

    refs #ticket-learning SpringBootApplication Initialization

---
+++ src/main/java/org/springboot/SpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootApplication.class, args);
+
+	}
+
+}
